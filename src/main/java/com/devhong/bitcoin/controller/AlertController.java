f1258dc5f8b2866e2246c5367216117e2a238606
sungjaeHong <sungjae89@gmail.com>
2018-01-20 18:11:30 +0900

    remove unused import

--- src/main/java/com/devhong/bitcoin/controller/AlertController.java
+++ src/main/java/com/devhong/bitcoin/controller/AlertController.java
@@ -1,6 +1,5 @@
 package com.devhong.bitcoin.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
--- src/main/java/com/devhong/bitcoin/controller/PriceController.java
+++ src/main/java/com/devhong/bitcoin/controller/PriceController.java
@@ -1,9 +1,6 @@
 package com.devhong.bitcoin.controller;
 
-import com.devhong.bitcoin.domain.model.PriceCoinResponse;
-import com.devhong.bitcoin.domain.model.bithumb.BithumbData;
 import com.devhong.bitcoin.domain.model.bithumb.BithumbResponse;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
